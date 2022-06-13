630eefdd2622e0f8deb4af31950375927090645f
cobalt88 <vincentteune@gmail.com>
2022-06-13 18:57:01 -0400

    6.13.22
    added in models

---
+++ .history/models/Pizza_20220613185308.js
@@ -0,0 +1,21 @@
+const { Schema, Model } = require('mongoose');
+
+const PizzaSchema = new Schema({
+  pizzaName: {
+    type: String
+  },
+  createdBy: {
+    type: String
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  size: {
+    type: String,
+    default: 'Large'
+  },
+  toppings: []
+});
+
+module.exports = Pizza
